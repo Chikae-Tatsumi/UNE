e14aa329b79e44ae23d004ee55b556b5364668f0
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2022-03-10 11:55:16 -0500

    Update Fig.1_metaMDS_16S_UNE.r

--- 3_Figures/Fig.1_metaMDS_16S_UNE.r
+++ 3_Figures/Fig.1_metaMDS_16S_UNE.r
@@ -51,7 +51,7 @@ ggplot()+
 geom_point(data=data, aes(x=MDS1,y=MDS2, color = DFB , size = DFE))+
 scale_colour_gradient(low="#f6766d",high="navy")+            
 geom_segment(data=env.arrows, aes(x = 0, y = 0, xend = (NMDS1*rate), yend = (NMDS2*rate)), arrow = arrow(length = unit(0.3,"cm")),color="black")+
-geom_text_repel(data=env.arrows, aes(x=(NMDS1*rate), y=(NMDS2*rate), label=rownames(env.arrows)),  size=6, color="black") +
+geom_text_repel(data=env.arrows, aes(x=(NMDS1*rate), y=(NMDS2*rate), label=rownames(env.arrows)),  size=5, color="black") +
 theme_classic()+
 theme(text=element_text(size=14,color="black"),axis.text=element_text(size=12,color="black"))+
 annotate("text", x=-Inf, y=-Inf, hjust=-0.01, vjust =-6, label=DFB.result, size=5) +
