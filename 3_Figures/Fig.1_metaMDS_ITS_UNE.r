aa5104851ed300e9d5e2fe4a271396bc6f122e8d
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2022-03-10 11:55:57 -0500

    Update Fig.1_metaMDS_ITS_UNE.r

--- 3_Figures/Fig.1_metaMDS_ITS_UNE.r
+++ 3_Figures/Fig.1_metaMDS_ITS_UNE.r
@@ -45,17 +45,17 @@ if (adonis[[1]][3,6] > 0.05) {DFBDFE.result <- ""
 }else {DFBDFE.result <- "        U×F ***    "}
 
 # ggplot
-rate = 0.2 # how many times shorter than acctual arrows
+rate = 0.3 # how many times shorter than acctual arrows
 ggplot()+
 geom_point(data=data, aes(x=MDS1,y=MDS2, color = DFB , size = DFE))+
 scale_colour_gradient(low="#f6766d",high="navy")+            
 geom_segment(data=env.arrows, aes(x = 0, y = 0, xend = (NMDS1*rate), yend = (NMDS2*rate)), arrow = arrow(length = unit(0.3,"cm")),color="black")+
-geom_text_repel(data=env.arrows, aes(x=(NMDS1*rate), y=(NMDS2*rate), label=rownames(env.arrows)),  size=6, color="black") +
+geom_text_repel(data=env.arrows, aes(x=(NMDS1*rate), y=(NMDS2*rate), label=rownames(env.arrows)),  size=5, color="black") +
 theme_classic()+
 theme(text=element_text(size=14,color="black"),axis.text=element_text(size=12,color="black"))+
-annotate("text", x=Inf, y=-Inf, hjust=0.9, vjust =-6, label=DFB.result, size=5) +
-annotate("text", x=Inf, y=-Inf, hjust=0.9, vjust =-4, label=DFE.result, size=5) +
-annotate("text", x=Inf, y=-Inf, hjust=0.9, vjust =-2, label=DFBDFE.result, size=5) 
+annotate("text", x=-Inf, y=Inf, hjust=-0.01, vjust =2, label=DFB.result, size=5) +
+annotate("text", x=-Inf, y=Inf, hjust=-0.01, vjust =4, label=DFE.result, size=5) +
+annotate("text", x=-Inf, y=Inf, hjust=-0.01, vjust =6, label=DFBDFE.result, size=5) 
 
 # Save
 ggsave(file = "NMDS_ITS.png",width =5, height =4)
