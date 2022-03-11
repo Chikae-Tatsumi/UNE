41f4bb9e23040ee489e1284bed985fad43d498d3
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2022-03-10 19:39:02 -0500

    Update F3_ASVtable_for_ECMonly.r

--- 1_Data_construction/F3_ASVtable_for_ECMonly.r
+++ 1_Data_construction/F3_ASVtable_for_ECMonly.r
@@ -1,5 +1,17 @@
 setwd("~/R/Analysis/2_UNE/ITS/FungalTrait")
 
+# For rarefied table
+fungaltrait.table <- read.csv(file="rarefied_fungaltrait.table.csv",row.names = 1)
+ASV <- fungaltrait.table [,1:(ncol(fungaltrait.table)-31)] 
+guild <- fungaltrait.table [,(ncol(fungaltrait.table)-24):ncol(fungaltrait.table)] 
+percent <- ASV / mean(colSums(ASV)) *100
+
+percent.table <- cbind (percent, guild)
+ECM.table<- percent.table[grep("ectomycorrhizal", percent.table$primary_lifestyle),]
+write.csv(ECM.table, "rarefied_ECM_table_FungalTrait.csv")
+
+
+# For full table
 fungaltrait.table <- read.csv(file="fungaltrait.table.csv",row.names = 1)
 ASV <- fungaltrait.table [,1:(ncol(fungaltrait.table)-31)] 
 guild <- fungaltrait.table [,(ncol(fungaltrait.table)-24):ncol(fungaltrait.table)] 
@@ -7,4 +19,4 @@ percent <- ASV / mean(colSums(ASV)) *100
 
 percent.table <- cbind (percent, guild)
 ECM.table<- percent.table[grep("ectomycorrhizal", percent.table$primary_lifestyle),]
-write.csv(ECM.table, "ASV.table.ECMonly.FungalTrait.csv")
+write.csv(ECM.table, "ECM_table_FungalTrait.csv")
