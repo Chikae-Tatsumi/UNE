dd19f9a591b8d0fca553332b56323ea2727ec248
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2022-03-31 17:50:40 -0400

    Add files via upload

---
+++ 2_Statistics/TableS4_lmer_Relative abundance_Phylum&Class_UNE.r
@@ -0,0 +1,46 @@
+library (lme4)
+library (lmerTest)
+
+setwd("~/R/Analysis/2_UNE")
+DESIGN <- read.csv("experimental_design.csv")
+setwd("~/R/Analysis/2_UNE/ITS") 
+objective <- read.csv("aggregated.phylum.class.table.csv", header=T, row.names=1)
+Basidomycota <- rowSums(objective[1:3])
+Ascomycota <- rowSums(objective[4:10])
+objective <- cbind(objective, Basidomycota, Ascomycota)
+
+NAME1 <- "Urbanization"
+NAME2 <- "Fragmentation"
+
+# ANOVA
+Results <- NULL
+for (i in 1:ncol(objective)){
+data <- cbind(DESIGN, objective[,i])
+colnames (data)[ncol(data)] <- colnames(objective)[i]
+data <- na.omit (data)
+model <- lmer(scale(data[,ncol(data)])~ scale(-DFB)*scale(-DFE)+(1|Site),data=data)
+anova <- anova(model)
+summary <- summary(model)
+
+estimate <- summary$coefficients[,1]
+Fval <- anova[,5]
+Pval <-anova[,6]
+Bind <- c(estimate[2], Fval[1], Pval[1], estimate[3], Fval[2], Pval[2], estimate[4],Fval[3], Pval[3])
+names(Bind) <- c(paste(NAME1,".R",sep=""),paste(NAME1,".F",sep=""),paste(NAME1,".P",sep=""),paste(NAME2,".R",sep=""),paste(NAME2,".F",sep=""),paste(NAME2,".P",sep=""),paste(NAME1,"*",NAME2,".R",sep=""),paste(NAME1,"*",NAME2,".F",sep=""),paste(NAME1,"*",NAME2,".P",sep=""))
+Results <- rbind(Results, Bind)}
+
+rownames(Results) <- colnames(objective)
+
+# P.val --> Asterisk
+Results.asterisk <- Results
+for (k in 1:ncol(Results)){
+if(k%%3 == 0){
+for (i in 1:nrow(Results)){
+if (Results[i,k] < 0.001) {Results.asterisk[i,k] <- "***"
+} else if (Results[i,k] < 0.01) {Results.asterisk[i,k] <- "**"
+} else if (Results[i,k] < 0.05) {Results.asterisk[i,k] <- "*"
+} else {Results.asterisk[i,k]<- "n.s"}}
+}}
+
+# Save
+write.csv(Results.asterisk, "lmer.ITS.phylum.class.csv") #Change
