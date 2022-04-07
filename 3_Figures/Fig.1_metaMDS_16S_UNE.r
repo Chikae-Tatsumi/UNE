ad98c49c97b31167f885de6ee74247fdf229e24d
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2022-04-06 20:24:17 -0400

    Update Fig.1_metaMDS_16S_UNE.r

--- 3_Figures/Fig.1_metaMDS_16S_UNE.r
+++ 3_Figures/Fig.1_metaMDS_16S_UNE.r
@@ -10,8 +10,8 @@ setwd("~/R/Analysis/2_UNE/16S")
 ASV.table <- read.table(file="rarefied_ASV_table.txt",header=T)
 
 # % table
-ASV <- ASV.table [,1:(ncol(ASV.table)-6)] # I have changed 7 into 6 because the table did not contain the Species column.
-taxonomy <- ASV.table [,(ncol(ASV.table)-5):ncol(ASV.table)] # Also, I have changed 6 into 5.
+ASV <- ASV.table [,1:(ncol(ASV.table)-6)] 
+taxonomy <- ASV.table [,(ncol(ASV.table)-5):ncol(ASV.table)] 
 percent <- ASV / mean(colSums(ASV)) *100
 
 # nmds
@@ -42,11 +42,11 @@ if (adonis[[1]][2,6] > 0.05) {DFE.result <- ""
 }else {DFE.result <- "        Fragmentation ***    "}
 if (adonis[[1]][3,6] > 0.05) {DFBDFE.result <- ""
 }else if (adonis[[1]][3,6] > 0.01) {DFBDFE.result <- "        U×F *      "
-}else if (adonis[[1]][3,6] > 0.001) {DFBDFE.result <- "        U×F**     "
+}else if (adonis[[1]][3,6] > 0.001) {DFBDFE.result <- "        U×F **     "
 }else {DFBDFE.result <- "        U×F ***    "}
 
 # ggplot
-rate = 0.3 # how many times shorter than acctual arrows
+rate = 0.5 # how many times shorter than acctual arrows
 ggplot()+
 geom_point(data=data, aes(x=MDS1,y=MDS2, color = DFB , size = DFE))+
 scale_colour_gradient(low="#f6766d",high="navy")+            
@@ -54,9 +54,9 @@ geom_segment(data=env.arrows, aes(x = 0, y = 0, xend = (NMDS1*rate), yend = (NMD
 geom_text_repel(data=env.arrows, aes(x=(NMDS1*rate), y=(NMDS2*rate), label=rownames(env.arrows)),  size=5, color="black") +
 theme_classic()+
 theme(text=element_text(size=14,color="black"),axis.text=element_text(size=12,color="black"))+
-annotate("text", x=-Inf, y=-Inf, hjust=-0.01, vjust =-6, label=DFB.result, size=5) +
-annotate("text", x=-Inf, y=-Inf, hjust=-0.01, vjust =-4, label=DFE.result, size=5) +
-annotate("text", x=-Inf, y=-Inf, hjust=-0.01, vjust =-2, label=DFBDFE.result, size=5) 
+annotate("text", x=-Inf, y=Inf, hjust=-0.01, vjust =2, label=DFB.result, size=5) +
+annotate("text", x=-Inf, y=Inf, hjust=-0.01, vjust =4, label=DFE.result, size=5) +
+annotate("text", x=-Inf, y=Inf, hjust=-0.01, vjust =6, label=DFBDFE.result, size=5) 
 
 # Save
 ggsave(file = "NMDS_16S.png",width =5, height =4)
