3af4dc62bf1f0edd33a45c3acee31c679ad4c0e0
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2022-04-06 20:58:36 -0400

    Update Fungi3_only_ECM_table_UNE.r

--- 1_Data_construction/Fungi3_only_ECM_table_UNE.r
+++ 1_Data_construction/Fungi3_only_ECM_table_UNE.r
@@ -3,5 +3,4 @@ setwd("~/R/Analysis/2_UNE/ITS/FungalTrait")
 # For rarefied table
 fungaltrait.table <- read.csv(file="rarefied_fungaltrait.table.csv",row.names = 1)
 ECM.table<- fungaltrait.table[grep("ectomycorrhizal", fungaltrait.table$primary_lifestyle),]
-ECM.table <- ECM.table [,1:(ncol(ECM.table)-24)] 
 write.csv(ECM.table, "rarefied_ECM_table_FungalTrait.csv")
