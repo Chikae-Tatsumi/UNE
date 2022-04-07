73957ea90cbdc75f349a9d06f9bb5c19d9990b6d
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2022-04-06 22:24:34 -0400

    Update TableS5_cor.test_tradeoff_FungalTrait_UNE.R

--- 2_Statistics/TableS5_cor.test_tradeoff_FungalTrait_UNE.R
+++ 2_Statistics/TableS5_cor.test_tradeoff_FungalTrait_UNE.R
@@ -4,12 +4,12 @@ DESIGN <- read.csv(file = "experimental_design.csv",header=T)
 setwd("~/R/Analysis/2_UNE/ITS/FungalTrait")
 data <- read.csv(file = "aggregated.fungaltrait.table.csv",header=T, row.names=1)
 
-Pros <- cbind(data$ECM.fungi, data$Saprotroph.fungi, 
-data$Soil.saportroph.fungi, data$Wood.saprotroph.fungi, data$Litter.saprotroph.fungi, data$Dung.saprotroph.fungi)
-Cons <- cbind(data$Plant.pathogenic.capacity,	data$Animal.parasite)
+Pros <- cbind(data$ECM, data$Saprotroph, 
+data$soil_saprotroph, data$wood_saprotroph, data$litter_saprotroph, data$dung_saprotroph)
+Cons <- cbind(data$Plant_pathogenic_capacity,	data$Animal_parasite)
 colnames(Pros) <- c("ECM fungi","Saprotroph fungi",	"Soil saportroph fungi",
 "Wood saprotroph fungi","Litter saprotroph fungi","Dung saprotroph fungi")
-colnames(Cons) <- c("Plant pathogenic capacity",	"Animal parasite")
+colnames(Cons) <- c("Plant pathogenic capacity", "Animal parasite")
 
 # Cor.test
 Result <- NULL
