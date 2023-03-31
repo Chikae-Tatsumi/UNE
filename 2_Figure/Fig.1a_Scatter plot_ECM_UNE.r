d782c2e6f4e0471fc96ba22e22326796a5e5d19f
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2023-03-31 15:00:00 -0400

    Update and rename Fig.1a_Scatter plot_ECM_UNE.r to 2_Figure/Fig.1a_Scatter plot_ECM_UNE.r

---
+++ 2_Figure/Fig.1a_Scatter plot_ECM_UNE.r
@@ -0,0 +1,27 @@
+library(ggplot2)
+library(lme4)
+library(lmerTest)
+
+# Import files
+setwd("~/R/Analysis/2_UNE")
+DESIGN <- read.csv("experimental_design.csv",header=T)
+setwd("~/R/Analysis/2_UNE/ITS/FungalTrait")
+fungaltrait <- read.csv(file="aggregated.fungaltrait.table.csv",header=T,row.names = 1)
+
+data <- cbind (fungaltrait, DESIGN)
+data$Urban <- factor (data$Urban, levels=c("Urban","Rural"))
+
+# ANOVA
+anova <- anova(lmer(scale(ECM)~ scale(DFB)*scale(DFE)+(1|Site),data=data))
+write.csv(anova, "ECM_rel.abund_anova.csv")
+
+# Visualize
+ggplot(data)+
+geom_point(aes(x=DFE, y=ECM, color=Urban),position=position_jitter( width=2, height=0))+ 
+geom_smooth(method="lm", aes(x=DFE, y=ECM, group=Urban, color=Urban))+  
+scale_color_manual(values = c("red","black"))+ 
+theme_classic()+
+theme(text=element_text(size=14,color="black"),axis.text=element_text(size=12,color="black"))+
+labs (y="Soil ECM fungi (%)", x="Distance from edge (m)",color="") 
+
+ggsave("ECM_rel.abund_UNE_nostats.png",width = 5, height = 4)
