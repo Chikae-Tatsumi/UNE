4249538aa6cf02e764461e13282067607669301d
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2023-04-14 13:58:36 -0400

    Update Fig.S3a_Line plot_ECMtree_UNE.r

--- 2_Figure/Fig.S3a_Line plot_ECMtree_UNE.r
+++ 2_Figure/Fig.S3a_Line plot_ECMtree_UNE.r
@@ -12,18 +12,19 @@ data <- cbind (METADATA, DESIGN)
 anova <- anova(lmer(scale(ECM.tree)~ scale(DFB)*scale(DFE)+(1|Site),data=data))
 write.csv(anova, "ECMtree_anova.csv")
 
-# Visualize
+# Format data
 standard_error <- function(x, na.rm = TRUE) {
   sd(x, na.rm = TRUE) / sqrt(length(na.omit(x)))}
 mean <- aggregate(METADATA, by=list(DESIGN$Urban,DESIGN$DFE),FUN= "mean", na.rm=TRUE)
 se <- aggregate(METADATA, by=list(DESIGN$Urban,DESIGN$DFE),FUN= standard_error)
 
-data <- cbind(mean[,1:2], mean$ECM.tree, se$ECM.tree)
-colnames(data) <- c("Urban","DFE","ECM.tree","se")
+vdata <- cbind(mean[,1:2], mean$ECM.tree, se$ECM.tree)
+colnames(vdata) <- c("Urban","DFE","ECM.tree","se")
 
-data$Urban <- factor (data$Urban, levels=c("Urban","Rural"))
+# Visualize
+vdata$Urban <- factor (vdata$Urban, levels=c("Urban","Rural"))
 
-ggplot(data)+
+ggplot(vdata)+
 geom_line(aes(x=DFE, y=ECM.tree, color=Urban, group=Urban))+
 geom_point(aes(x=DFE, y=ECM.tree, color=Urban, group=Urban), 
 size=2,position=position_dodge(0.2))+
