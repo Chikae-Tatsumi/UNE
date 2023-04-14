f5aee6f7d5d04763a235f7b24e566157615ac451
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2023-04-14 11:46:05 -0400

    Delete Fungi03_ECM_table_UNE.r

--- 1_Data_construction/Fungi03_ECM_table_UNE.r
+++
@@ -1,6 +0,0 @@
-setwd("~/R/Analysis/2_UNE/ITS/FungalTrait")
-
-# For rarefied table
-fungaltrait.table <- read.csv(file="rarefied_fungaltrait.table.csv",row.names = 1)
-ECM.table<- fungaltrait.table[grep("ectomycorrhizal", fungaltrait.table$primary_lifestyle),]
-write.csv(ECM.table, "rarefied_ECM_table_FungalTrait.csv")
