24dad5dae2e5344a8d4903e1f6f565bbab44a79d
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2023-04-14 11:53:24 -0400

    Add files via upload

---
+++ 2_Figure/Fig.1a_Line plot_ECM_UNE.r
@@ -0,0 +1,36 @@
+library(ggplot2)
+library(lme4)
+library(lmerTest)
+
+# Import files
+setwd("~/R/Analysis/2_UNE")
+DESIGN <- read.csv("experimental_design.csv",header=T)
+setwd("~/R/Analysis/2_UNE/ITS/FungalTrait")
+fungaltrait <- read.csv(file="aggregated.fungaltrait.table.csv",header=T,row.names = 1)
+
+# ANOVA
+data <- cbind (fungaltrait, DESIGN)
+anova <- anova(lmer(scale(ECM)~ scale(DFB)*scale(DFE)+(1|Site),data=data))
+write.csv(anova, "ECM_rel.abund_anova.csv")
+
+# Visualize
+standard_error <- function(x) sd(x) / sqrt(length(x)) 
+mean <- aggregate(fungaltrait, by=list(DESIGN$Urban,DESIGN$DFE),FUN= "mean")
+se <- aggregate(fungaltrait, by=list(DESIGN$Urban,DESIGN$DFE),FUN= standard_error)
+
+data <- cbind(mean[,1:2], mean$ECM, se$ECM)
+colnames(data) <- c("Urban","DFE","ECM","se")
+
+data$Urban <- factor (data$Urban, levels=c("Urban","Rural"))
+
+ggplot(data)+
+geom_line(aes(x=DFE, y=ECM, color=Urban, group=Urban))+
+geom_point(aes(x=DFE, y=ECM, color=Urban, group=Urban), 
+size=2,position=position_dodge(0.2))+
+geom_errorbar(aes(x=DFE, ymin=ECM-se, ymax=ECM+se, color=Urban, group=Urban),position=position_dodge(0.2)) +
+scale_color_manual(values = c("red","black"))+ 
+theme_classic()+
+theme(text=element_text(size=14,color="black"),axis.text=element_text(size=12,color="black"))+
+labs (y="Soil ECM fungi (%)", x="Distance from edge (m)") 
+
+ggsave("ECM_rel.abund_UNE.png",width = 4, height = 3)
---
+++ 2_Figure/Fig.1c_Line plot_Pathogen_UNE.r
@@ -0,0 +1,54 @@
+library(ggplot2)
+library(lme4)
+library(lmerTest)
+
+# Import files
+setwd("~/R/Analysis/2_UNE")
+DESIGN <- read.csv("experimental_design.csv",header=T)
+setwd("~/R/Analysis/2_UNE/ITS/FungalTrait")
+fungaltrait <- read.csv(file="aggregated.fungaltrait.table.csv",header=T,row.names = 1)
+
+# ANOVA
+data <- cbind (fungaltrait, DESIGN)
+
+anova <- anova(lmer(scale(Plant_pathogenic_capacity)~ scale(DFB)*scale(DFE)+(1|Site),data=data))
+write.csv(anova, "Plant_pathogen_anova.csv")
+
+anova <- anova(lmer(scale(Animal_parasite)~ scale(DFB)*scale(DFE)+(1|Site),data=data))
+write.csv(anova, "Animal_parasite_anova.csv")
+
+# Format data
+Plant <- data.frame(cbind(DESIGN$Urban, DESIGN$DFE, "Plant", fungaltrait$Plant_pathogenic_capacity))
+Animal <- data.frame(cbind(DESIGN$Urban, DESIGN$DFE, "Animal",fungaltrait$Animal_parasite))
+
+pathogen <- data.frame(rbind(Plant, Animal))
+colnames(pathogen) <- c("Urban","DFE","Type","Pathogenic_capacity")
+pathogen.numeric <- as.numeric(pathogen$Pathogenic_capacity)
+
+standard_error <- function(x) sd(x) / sqrt(length(x)) 
+mean <- aggregate(pathogen.numeric, by=list(pathogen$Urban,pathogen$DFE,pathogen$Type),FUN= "mean")
+se <- aggregate(pathogen.numeric, by=list(pathogen$Urban,pathogen$DFE,pathogen$Type),FUN= standard_error)
+
+data <- cbind(mean[,1:3], mean$x, se$x)
+colnames(data) <- c("Urban","DFE","Type","pathogen","se")
+
+data$DFE <- as.numeric(data$DFE)
+data$Type_Urban <- paste(data$Type, data$Urban, sep="-")
+
+# Visualize
+data$Urban <- factor (data$Urban, levels=c("Urban","Rural"))
+data$Type <- factor (data$Type, levels=c("Plant","Animal"))
+data$Type_Urban <- factor (data$Type_Urban, levels=c("Plant-Urban","Plant-Rural","Animal-Urban","Animal-Rural"))
+
+ggplot(data)+
+geom_line(aes(x=DFE, y=pathogen, color=Urban, linetype=Type, group=Type_Urban))+
+geom_point(aes(x=DFE, y=pathogen, color=Urban, shape=Type, group=Type_Urban), 
+size=2,position=position_dodge(0.2))+
+geom_errorbar(aes(x=DFE, ymin=pathogen-se, ymax=pathogen+se, color=Urban, group=Type_Urban),position=position_dodge(0.2)) +
+scale_color_manual(values = c("red","black"))+  
+scale_shape_manual(values = c(16,2))+  
+theme_classic()+
+theme(text=element_text(size=14,color="black"),axis.text=element_text(size=14,color="black"))+
+labs (y="Soil pathogenic fungi (%)", x="Distance from edge (m)", color=NULL, shape=NULL, linetype=NULL)  
+
+ggsave("Pathogen_rel.abund_UNE.png",width = 4, height = 3)
